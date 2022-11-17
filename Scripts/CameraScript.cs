acbb65405fbd2496e04b45ca69d86973cabb33d2
unknown <spd2020.work@gmail.com>
2022-11-17 18:57:59 +0500

    add camera movement control

--- Scripts/CameraScript.cs
+++ Scripts/CameraScript.cs
@@ -1,28 +1,85 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 public class CameraScript : MonoBehaviour
 {
-    public float mainSpeed = 100.0f;
-    public float camSens = 0.25f;
+    public GameObject MoveCameraPanel; // Панель кнопок перемешения камеры
 
-    public bool rotateOnlyIfMousedown = true;
+    public Transform target; // объект вокруг которого должна крутиться камера
 
-    private Vector3 lastMouse = new Vector3(255, 255, 255);
+    public float mainSpeed = 100.0f; // скорость перемешения камеры
+    public float camSens = 0.25f; // скарость врашения камеры
+
+    public bool rotateOnlyIfMousedown = false; // должна ли варщаться камера 
+
+    private Vector3 StartPositionCamera = new Vector3(0, 5, 0);
+
+    private Vector3 lastMouse = new Vector3(255, 255, 255); 
+
+    public void CameraONMoveButton()
+    {
+        if (rotateOnlyIfMousedown)
+            rotateOnlyIfMousedown = false;
+        else if (!rotateOnlyIfMousedown)
+            rotateOnlyIfMousedown = true;
+    }
+
+    public void MoveCameraPanelSetActive()
+    {
+        if (MoveCameraPanel.active)
+            MoveCameraPanel.SetActive(false);
+        else if (!MoveCameraPanel.active)
+            MoveCameraPanel.SetActive(true);
+    }
+
+    public void GetButtonUP()
+    {
+        if (rotateOnlyIfMousedown && Camera.main.transform.position.z < 16)
+        {
+            Camera.main.transform.position += new Vector3(0, 0, 1);
+        }
+    }
+
+    public void GetButtonDOWN()
+    {
+        if (rotateOnlyIfMousedown && Camera.main.transform.position.z > -16)
+        {
+            Camera.main.transform.position += new Vector3(0, 0, -1);
+        }
+    }
+
+    public void GetButtonLEFT()
+    {
+        if (rotateOnlyIfMousedown && Camera.main.transform.position.x > -16)
+        {
+            Camera.main.transform.position += new Vector3(-1, 0, 0);
+        }
+    }
+
+    public void GetButtonRIGHT()
+    {
+        if (rotateOnlyIfMousedown && Camera.main.transform.position.x < 16)
+        {
+            Camera.main.transform.position += new Vector3(1, 0, 0);
+        }
+    }
 
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
             lastMouse = Input.mousePosition;
-        if (!rotateOnlyIfMousedown || rotateOnlyIfMousedown && Input.GetMouseButton(0))
+        if (/*!rotateOnlyIfMousedown ||*/ rotateOnlyIfMousedown && Input.GetMouseButton(0))
         {
             lastMouse = Input.mousePosition - lastMouse;
             lastMouse = new Vector3(-lastMouse.y * camSens, lastMouse.x * camSens, 0);
-            lastMouse = new Vector3(transform.eulerAngles.x + lastMouse.x, transform.eulerAngles.y + lastMouse.y, 0);
-            transform.eulerAngles = lastMouse;
+            lastMouse = new Vector3(Camera.main.transform.eulerAngles.x + lastMouse.x, Camera.main.transform.eulerAngles.y + lastMouse.y, 0);
+            Camera.main.transform.eulerAngles = lastMouse;
             lastMouse = Input.mousePosition;
         }
+
+    
     }
 }
