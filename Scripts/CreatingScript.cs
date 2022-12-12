7d3013dbe8644d58f0b5ca5702eeb7c5c33743a0
unknown <spd2020.work@gmail.com>
2022-12-13 04:29:02 +0500

    little change

--- Scripts/CreatingScript.cs
+++ Scripts/CreatingScript.cs
@@ -7,7 +7,6 @@ public class CreatingScript : MonoBehaviour
     private Vector3 pos = new Vector3(0, 5, 0);
     public void CreatingBatton(GameObject obj)
     { 
-        Vector3 pos = new Vector3(Random.Range(-10f, 10f), Random.Range(0.4f, 4f), Random.Range(-10f, 10f));
         Instantiate(obj, pos, Quaternion.identity);
     }
     // Start is called before the first frame update
